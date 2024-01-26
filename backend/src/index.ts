3492fef8ad201ce0031d380d7f494440942ce352
Nothile Moyo <Nothile1@gmail.com>
2024-01-26 21:10:48 +0000

    Progress and created the connection file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,30 @@
-const text : string = "Hello";
+/**
+ * Date created: 26/01/2024
+ * Author: Nothile Moyo
+ * 
+ * The index file. This serves as our main method.
+ * We start our server and create our connection pool here
+ * 
+ * @method startServer : async () => Promise<void>
+ * @method generateCSRFToken : () => number
+ */
 
-console.log(text);
+// Main imports in order to run the server
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import { createMongooseConnection, SESSION_URI, MONGODB_USERNAME } from "./data/connection";
+
+// Module augmentation for the request
+declare module 'express-serve-static-core' {
+    interface Request{
+        fileName : string,
+        isAuthenticated : boolean,
+    }
+}
+
+// Import the .env variables
+dotenv.config();
+
+// Be able to instantiate our express server
+const app = express();
