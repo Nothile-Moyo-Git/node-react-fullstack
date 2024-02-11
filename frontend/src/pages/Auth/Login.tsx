b2e972c9c2d715d1bb23ef47a8ee815aca78ec12
Nothile Moyo <Nothile1@gmail.com>
2024-02-11 18:11:18 +0000

    Login form initially rendered

---
+++ frontend/src/pages/Auth/Login.tsx
@@ -0,0 +1,53 @@
+/**
+ * Date created : 11/02/2024
+ * 
+ * Author : Nothile Moyo
+ */
+
+import { FC, useState, useRef, FormEvent } from "react";
+
+export const LoginPage : FC = () => {
+
+    // Setting up the states
+    const [isEmailValid, setIsEmailValid] = useState<boolean>(true);
+    const [isPasswordValid, setIsPasswordValid] = useState<boolean>(true);
+
+    // Set up refs so we can reference our inputs. We use refs instead of state for performance optimizations
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = (event : FormEvent) => {
+
+        // Don't reload the page
+        event.preventDefault();
+
+
+    };
+
+    return(
+        <section>
+
+            <form onSubmit={submitHandler}>
+
+                <div>
+                    <label>Email*</label>
+                    <input
+                        type="text"
+                        ref={emailRef}
+                    />
+                </div>
+
+                <div>
+                    <label>Password*</label>
+                    <input
+                        type="password"
+                        ref={passwordRef}
+                    />
+                </div>
+
+                <button>Submit</button>
+            </form>
+
+        </section>
+    );
+};
