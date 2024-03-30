0bc2a21553f31842ced261fbf0994779d492d9bf
Nothile Moyo <Nothile1@gmail.com>
2024-03-30 23:15:44 +0000

    progress

--- frontend/src/pages/Posts/CreatePost.tsx
+++ frontend/src/pages/Posts/CreatePost.tsx
@@ -8,10 +8,16 @@
  * Only logged in users will be able to create posts in the backend
  */
 
-import { FC, useContext, useEffect, useRef } from "react";
+import { FC, FormEvent, useContext, useEffect, useState, useRef } from "react";
 import { AppContext } from "../../context/AppContext";
 import { useNavigate } from "react-router-dom";
+import Form from "../../components/form/Form";
+import Title from "../../components/form/Title";
+import Label from "../../components/form/Label";
+import Input from "../../components/form/Input";
+import Field from "../../components/form/Field";
 
+import "./CreatePost.scss";
 
 export const CreatePostComponent : FC = () => {
 
@@ -25,25 +31,67 @@ export const CreatePostComponent : FC = () => {
     const titleRef = useRef<HTMLInputElement>(null);
     const imageUrlRef = useRef<HTMLInputElement>(null);
     const content = useRef<HTMLInputElement>(null);
+
+    const [isPasswordValid, setIsPasswordValid] = useState<boolean>(true);
   
     // Check authentication when component mounts
     useEffect(() => {
 
         appContextInstance?.validateAuthentication();
 
-        console.clear();
-        console.log("App context instance");
-        console.log(appContextInstance?.userAuthenticated);
-
         // If the user is authenticated, redirect this route to the previous page
         !appContextInstance?.userAuthenticated && navigate(-1);
 
     },[appContextInstance, navigate]); 
 
+    // Handle the form submission for creating a new post
+    const submitHandler = (event : FormEvent) => {
+
+        event.preventDefault();
+    };
+
     return (
-        <>
-            <h1>Create Post</h1>
-        </>
+        <section className="createPost">
+
+                <Form onSubmit={submitHandler}>
+
+                    <Title>Create Post</Title>
+
+                    <Field>
+                        <Label
+                            htmlFor="title"
+                            id="titleLabel"
+                            errorText="Error: Title must be longer than 3 characters"
+                        >Title*</Label>
+                        <Input
+                            ariaLabelledBy="titleLabel "
+                            error={!isPasswordValid}
+                            name="title"
+                            placeholder="Please enter your title"
+                            ref={titleRef}
+                            type="text"
+                        />
+                    </Field>
+
+                    <Field>
+                        <Label
+                            htmlFor="imageUrl"
+                            id="imageUrlLabel"
+                            errorText="Error: File type is invalid"
+                        >Title*</Label>
+                        <Input
+                            ariaLabelledBy="imageUrlLabel"
+                            error={!isPasswordValid}
+                            name="image"
+                            placeholder="Please enter your title"
+                            ref={titleRef}
+                            type="file"
+                        />
+                    </Field>
+
+                </Form>
+
+        </section>
     );
 };
 
