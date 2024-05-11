1ecc2b5663109604891c8db4f6e16596b7e959ad
Nothile Moyo <Nothile1@gmail.com>
2024-05-11 22:15:33 +0100

    Implementing skeleton of the confirmation modal.
    Renamed some of the files to make them more appropriate.
    
    Added descriptions for certain components for ease of use
    
    Toggling the confirmation modal now works

--- frontend/src/pages/Posts/PostScreen.tsx
+++ frontend/src/pages/Posts/PostScreen.tsx
@@ -19,7 +19,7 @@ import { BASENAME, generateUploadDate } from "../../util/util";
 import { MdKeyboardBackspace } from "react-icons/md";
 import Button from "../../components/button/Button";
 import LoadingSpinner from "../../components/loader/LoadingSpinner";
-import ErrorModal from "../../components/error/ErrorModal";
+import ErrorModal from "../../components/modals/ErrorModal";
 
 const PostScreen : FC = () => {
 
