165afd6f3deb064602a6d0ead74ca7c1005588ae
Nothile Moyo <Nothile1@gmail.com>
2024-06-05 22:13:52 +0100

    Worked on socket.io

--- frontend/src/pages/Sockets/LiveChat.tsx
+++ frontend/src/pages/Sockets/LiveChat.tsx
@@ -8,13 +8,32 @@
  * This will apply to both the front and the backend
  */
 
-import { FC } from "react";
+import { DefaultEventsMap } from "@socket.io/component-emitter";
+import { FC, useEffect } from "react";
+import { io, Socket } from "socket.io-client";
 
 const LiveChat : FC = () => {
 
+    let socket: Socket<DefaultEventsMap, DefaultEventsMap> | null = null;
+
+    useEffect(() => {
+
+        console.log("Component rendered");
+
+        // Connect to the websocket if the socket doesn't exist already
+        console.log("Socket");
+        console.log(socket);
+    });
+
+    if (!socket) {
+        socket = io("http://localhost:4000");
+    }
+
+
+
     return(
         <section>
-            Livechat
+            <h1>Live chat</h1>
         </section>
     );
 };
