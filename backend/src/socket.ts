626b1f4ad13d7fbbf85210634ec4a47fb643992c
Nothile Moyo <Nothile1@gmail.com>
2024-06-30 21:46:12 +0100

    Worked on socket io class

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -7,39 +7,49 @@
  * description : Our socket connection in a file which can be shared amongst multiple files
  */
 
-import { Server, IncomingMessage, ServerResponse } from "http";
+import { Server, IncomingMessage, ServerResponse, RequestListener } from "http";
+import { ServerOptions, Server as Socket } from "socket.io";
 
 interface SocketOptionsProps {
-    cors : {
-        origin : string
-    }
+
 }
 
-class SocketIO {
+/**
+ * @class ClassSocketIO
+ * 
+ * @param server: Server<typeof IncomingMessage, typeof ServerResponse>
+ * @param options: SocketOptionsProps
+ * 
+ * @description A socket class to handle connection functionality which can be shared amongst multiple files
+ */
 
-    server: Server<typeof IncomingMessage, typeof ServerResponse>;
-    options: SocketOptionsProps;
 
-    constructor(
+class ClassSocketIO {
+
+    // Define our properties here
+    protected server: Server<typeof IncomingMessage, typeof ServerResponse>;
+    protected options;
+
+    // Constructor to start the server
+    public constructor(
         server: Server<typeof IncomingMessage, typeof ServerResponse>, 
-        options: SocketOptionsProps
+        options
     ){
         this.server = server;
         this.options = options;
+
+        if (this.server) {
+            // Create our connection to socket.io
+            // const socketIO = new Server(this.server, this.options);
+        }
+    }
+
+    protected getIO() {
+        if (this.server) {
+
+        }
     }
 }
 
-const handleSocketConnection = (
-    server : Server<typeof IncomingMessage, typeof ServerResponse>, 
-    options : SocketOptionsProps
-) => {
-
-    console.log("\n");
-    console.log("Options");
-    console.log(options);
-
-
-};
-
-export default handleSocketConnection;
+export default ClassSocketIO;
 
