aa76234da3dddbfdd22d46e31ce3333f1e72002a
Nothile Moyo <Nothile1@gmail.com>
2024-09-21 23:49:54 +0100

    Insert movie query now works as expected

--- backend/src/data/resolvers/AuthResolvers.ts
+++ backend/src/data/resolvers/AuthResolvers.ts
@@ -47,43 +47,55 @@ const getDocument = async () => {
 };
 
 // Add a movie to the backend
-const insertMovie = async () => {
+const insertMovie = async (parent, args) => {
 
     const name = "Pulp Fiction";
     const description = "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.";
     const year = 1994;
 
-    console.log("\n\n");
-    console.log("Insert movie");
-    console.log("\n\n");
+    try {
 
-    // Perform a query to add an entry to our database as a document
-    const response = await fetch(`${API_ENDPOINT}/action/insertOne`, {
-        method : 'POST',
-        headers : {
-            'Content-Type': 'applcation/json',
-            'api-key': DATA_API_KEY
-        },
-        body: JSON.stringify({
-            collection : "movies",
-            database : "backend",
-            dataSource: "backend",
-            document : {
-                name : name,
-                description : description, 
-                year : String(year)
-            }
-        }),
-    });
+        // Perform a query to add an entry to our database as a document
+        const response = await fetch(`${API_ENDPOINT}/action/insertOne`, {
+            method : 'POST',
+            headers : {
+                'Content-Type': 'application/json',
+                'api-key': DATA_API_KEY
+            },
+            body: JSON.stringify({
+                collection : "movies",
+                database : "backend",
+                dataSource: "backend",
+                document : {
+                    name : name,
+                    description : description, 
+                    year : String(year)
+                }
+            }),
+        });
 
-    const result = await response.json();
+        // If our request was successful
+        if (response.status === 201) {
 
-    console.log("\n\n");
-    console.log("Result");
-    console.log(result);
-    console.log("\n\n")
+            // If the request is successful then return a response
+            const result = await response.json();
 
-    return result;
+            return { result : `Object ${result.insertedId} inserted` };
+        }else{
+
+            return { result : `No object inserted` };
+        }
+
+    }catch(error){
+
+        // Output our errors to both the server and the front end
+        console.log("\n\n");
+        console.log("Auth/InsertMovie query failed, error below");
+        console.log(error);
+        console.log("\n\n");
+
+        return { result : `Error : Query failed, please look at your server logs` }
+    }
 
 };
 
