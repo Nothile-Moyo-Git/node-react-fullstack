783385b2fb3c89cbb10fa048a194e115998e7d7c
Nothile Moyo <Nothile1@gmail.com>
2024-10-25 23:13:34 +0100

    started working on chat query

--- backend/src/data/schemas/ChatSchema.ts
+++ backend/src/data/schemas/ChatSchema.ts
@@ -8,3 +8,19 @@
  */
 
 import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList, GraphQLBoolean } from 'graphql';
+import chatResolvers from '../resolvers/ChatResolvers';
+
+const ChatQuery = new GraphQLObjectType({
+    name : 'ChatQuery',
+    fields : {
+        chatMessagesResponse : {
+            type : new GraphQLObjectType({
+                name : "getChatMessages",
+                fields : {
+                    succcess : { type : GraphQLBoolean }
+                }
+            }),
+            resolve : chatResolvers.GetChatsResolver
+        }
+    }
+})
