1fef34b9ac3b845071480d0ad9b76b5c29de42f9
Nothile Moyo <Nothile1@gmail.com>
2024-11-01 23:44:47 +0000

    Made progress with posts

---
+++ backend/src/data/schemas/PostSchema.ts
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Author : Nothile Moyo
+ * 
+ * Date Created : 01/11/2024
+ * 
+ * Description : Handle the GraphQL requests for the post middleware. This contains sessions and users
+ * 
+ */
+
+import { GraphQLSchema, GraphQLObjectType, GraphQLString } from "graphql";
+import postResolvers from "../resolvers/PostResolvers";
+
+
+
+
+
